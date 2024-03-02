24add65534021a7dd891bd0958f3dd49f1fac2c1
hasanraza38 <92hasanraza689@gmail.com>
2024-03-03 03:28:29 +0500

    js-assignment

---
+++ app.js
@@ -0,0 +1,56 @@
+// chapter # 1 
+
+// Q-1
+// Q-2
+// alert("Error!please enter avalid password")
+
+
+// Q-3
+// Q-4
+
+// alert("Welcome To JS land... \nHappy Coding!" )
+
+// Q-5
+
+// Hello....I can Run JS through my web browser's console 
+
+// chapter # 2
+
+// Q-1
+// var UserName = "hasanraza38";
+// console.log(UserName);
+
+// Q-2
+
+// var MyName = "Muhammad Hasan Raza";
+// console.log(MyName);
+
+// Q-3
+
+// var message= "Hello world"
+// alert(message)
+
+// Q-4
+
+// var UserName= 'john';
+// var age= "15 years old";
+// var UserIntro="Certified Mobile Application Developer";
+// alert(UserName);
+// alert(age);
+// alert(UserIntro);
+
+
+// Q-5
+
+// Q-6
+
+// var Email="92hasanraza@gmail.com";
+// alert( Email );
+
+// Q-7
+
+// var book= "A Smarter Way To learn Js";
+// alert(book);
+
+// Q-8
+// document.write("Yah! I can write HTML content through Javascript  ")
